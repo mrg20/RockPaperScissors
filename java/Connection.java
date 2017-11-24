ab94075589fcbdd10021b5435e734e54f89bfa09
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-24 19:04:05 +0100

    connection code done, need to review the exception handling later

--- java/Connection.java
+++ java/Connection.java
@@ -7,26 +7,47 @@ public class Connection extends Thread{
     private DataInputStream input;
     private DataOutputStream output;
     private Socket serviceSocket;
+    private GameController gameController;
 
     public Connection(Socket serviceSocket){
         this.serviceSocket = serviceSocket;
+        gameController = new GameController();
     }
 
     @Override
     public void run(){
-
+        try {
+            openInputSocket();
+            openOutputSocket();
+            inputOutputController();
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                serviceSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 
     public void openInputSocket() throws IOException {
         input = new DataInputStream(serviceSocket.getInputStream());
     }
 
-    public String readSocket() throws IOException {
-        return input.readUTF();
-    }
-
     public void openOutputSocket() throws IOException {
         output = new DataOutputStream(serviceSocket.getOutputStream());
+    }
+
+    private void inputOutputController() throws IOException {
+        while (true) {
+            String clientInfo = readSocket();
+            String matchResult = gameController.getMatchResult(clientInfo);
+            sendInformation(matchResult);
+        }
+    }
+
+    public String readSocket() throws IOException {
+        return input.readUTF();
     }
 
     public void sendInformation(String information) throws IOException {
--- java/Server.java
+++ java/Server.java
@@ -12,7 +12,9 @@ public class Server {
     public void startConnectionManagment() throws IOException {
         while (true) {
             Socket clientSocket = socketOnService.accept();
+            System.out.println("Client Accepted");
             new Connection(clientSocket).start();
+            System.out.println("Connection Started");
         }
     }
 }
