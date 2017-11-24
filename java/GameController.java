8676ffccd85c81782dd17b012e7565d14c0a2b90
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-24 19:06:20 +0100

    added the game controller

---
+++ java/GameController.java
@@ -0,0 +1,10 @@
+public class GameController {
+
+    public void printInstructions(){
+        System.out.println("Game Instructions");
+    }
+
+    public String getMatchResult(String clientInfo){
+        return "";
+    }
+}
--- java/RockPaperScissors.java
+++ java/RockPaperScissors.java
@@ -2,13 +2,9 @@ import java.io.IOException;
 
 public class RockPaperScissors {
     public static void main(String[] args) throws IOException {
-        giveInstructions();
+        new GameController().printInstructions();
         Server server = new Server();
         server.openSocket();
         server.startConnectionManagment();
-    }
-
-    private static void giveInstructions() {
-        System.out.println("Instructions");
     }
 }
