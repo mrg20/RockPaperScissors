f4ce2878dedb5bc4cf6158984c02e414a0546f0e
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-24 18:30:07 +0100

    updated the reads and writes, now using UTF8

--- java/Server.java
+++ java/Server.java
@@ -8,6 +8,8 @@ import java.net.Socket;
 public class Server {
     private ServerSocket myService;
     private Socket serviceSocket;
+    private DataInputStream input;
+    private DataOutputStream output;
 
     public void openSocket() throws IOException {
         myService = new ServerSocket(1024);
@@ -17,20 +19,24 @@ public class Server {
         try {
             serviceSocket = myService.accept();
         }
-        catch (IOException e) {//Do not want to close the socket if I cannot accept a client.
-            System.out.println(e);
+        catch (IOException acceptClientError) {//Do not want to close the socket if I cannot accept a client.
+            System.out.println(acceptClientError);
         }
     }
 
-    public DataInputStream receiveInformation() throws IOException {
-        DataInputStream input = null;
+    public void openInputSocket() throws IOException {
         input = new DataInputStream(serviceSocket.getInputStream());
-        return input;
+    }
+
+    public String readSocket() throws IOException {
+        return input.readUTF();
+    }
+
+    public void openOutputSocket() throws IOException {
+        output = new DataOutputStream(serviceSocket.getOutputStream());
     }
 
     public void sendInformation(String information) throws IOException {
-        DataOutputStream output;
-        output = new DataOutputStream(serviceSocket.getOutputStream());
-        output.writeBytes(information);
+        output.writeUTF(information);
     }
 }
