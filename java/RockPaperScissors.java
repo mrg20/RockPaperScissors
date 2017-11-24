b187e686ebe06d2db5e853967a41272d60621ec6
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-24 15:12:17 +0100

    Introduced the java client

---
+++ java/RockPaperScissors.java
@@ -0,0 +1,8 @@
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        //Instructions
+        //Ask and get action
+        //Send action to the server
+        //Print win or lose
+    }
+}
