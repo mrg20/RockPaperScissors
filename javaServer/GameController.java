884ee15edc16fac627af2eb7fea972e9001ef185
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-25 18:42:58 +0100

    Game ended

--- javaServer/GameController.java
+++ javaServer/GameController.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class GameController {
 
     public void printInstructions(){
@@ -5,6 +7,24 @@ public class GameController {
     }
 
     public String doMatch(String clientInfo){
-        return "data";
+        int action = createAction();
+        return compareActions(action, Integer.parseInt(clientInfo));
+    }
+
+    private int createAction() {
+        Random random = new Random();
+        int max = 3;
+        int min = 1;
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    private String compareActions(int action, int clientAction) {
+        int[] winList=new int[]{3, 1, 2};
+        if(winList[clientAction-1] == action){
+            return "YOU WIN THIS TIME";
+        }else if(action == clientAction){
+            return "DRAFT, USED THE SAME COMMANDS";
+        }
+        return "YOU LOSE, THIS AI IS POWERFUL";
     }
 }
