a251f1bad6ab41bfaa5cc7a2b2f30626ae1c0056
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-25 16:35:22 +0100

    refactored into classes

---
+++ pythonClient/Game.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+class Game:
+    def instructions(self):
+        print("instructions")
+
+    def notify_result(self, response):
+        pass
+
+    def process_action(self, action):
+        pass
--- pythonClient/RockPaperScissors.py
+++ pythonClient/RockPaperScissors.py
@@ -1,16 +1,31 @@
+# -*- coding: utf-8 -*-
 
-def open_socket():
-    service_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    service_socket.connect(('localhost', 9000))
+from __future__ import print_function
+import socket
+from Game import Game
 
-def main():
-    instructions()
-    open_socket()
-    while(True):
-        print "Enter an action:",
-        action = raw_input()
-        response = send_action()
-        print response
+
+class Client:
+    def open_socket(self):
+        print("Opening the socket")
+        self.service_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.service_socket.connect(('localhost', 9000))
+
+    def send_action(self, data):
+        self.service_socket.send(data)
+        received_data = self.service_socket.recv(512)
+        print(received_data)
+        return received_data
+
 
 if __name__ == '__main__':
-    main()
+    client = Client()
+    game = Game()
+    game.instructions()
+    client.open_socket()
+    while (True):
+        print("Enter an action:")
+        action = raw_input()
+        info_to_send = game.process_action(action)
+        response = client.send_action(info_to_send)
+        game.notify_result(response)
