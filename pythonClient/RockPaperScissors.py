c29294c6a3eebd1c26a24fa5e37eef87a4dae84f
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-25 16:05:06 +0100

    Following the guidestyle

--- pythonClient/RockPaperScissors.py
+++ pythonClient/RockPaperScissors.py
@@ -1,13 +1,15 @@
 
-
+def open_socket():
+    service_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    service_socket.connect(('localhost', 9000))
 
 def main():
     instructions()
-    openSocket()
+    open_socket()
     while(True):
         print "Enter an action:",
         action = raw_input()
-        response = sendAction()
+        response = send_action()
         print response
 
 if __name__ == '__main__':
